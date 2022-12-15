deaf47ba1e1babce301b45e7c4817320f068370e
diegotimao <diegotimao1104@gmail.com>
2022-12-15 16:32:11 -0500

    feat: Incializando a classe SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,11 @@
 class SimpleReport:
-    pass
+    def __init__(
+        self, 
+        id, 
+        nome_do_produto, 
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    ):
