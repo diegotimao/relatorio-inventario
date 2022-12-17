99c13a04bd31dbcf64363b78d0cc8987f2e7c4cb
diegotimao <diegotimao1104@gmail.com>
2022-12-17 16:07:51 -0500

    feat: Gerando a versão simplificada do relatório

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,11 +1,31 @@
+from typing import List, Dict
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    def __init__(
-        self, 
-        id, 
-        nome_do_produto, 
-        nome_da_empresa,
-        data_de_fabricacao,
-        data_de_validade,
-        numero_de_serie,
-        instrucoes_de_armazenamento,
-    ):
+    @staticmethod
+    def generate(list: List[Dict]):
+        data_atual = datetime.now()
+        validade = []
+        data_antiga = []
+        companhia = []
+
+        for item in list:
+            data_antiga.append(item["data_de_fabricacao"])
+            companhia.append(item["nome_da_empresa"])
+
+            if (
+                datetime.strptime(item["data_de_validade"], "%Y-%m-%d")
+                >= data_atual
+            ):
+                validade.append(item["data_de_validade"])
+
+        empresa = max(Counter(companhia))
+        companhia_name = empresa.replace(" LIMITED", "")
+
+        return (
+            f"Data de fabricação mais antiga: {min(data_antiga)}\n"
+            f"Data de validade mais próxima: {min(validade)}\n"
+            f"Empresa com mais produtos: {companhia_name}"
+        )
