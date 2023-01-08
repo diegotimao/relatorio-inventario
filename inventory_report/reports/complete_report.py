f4785cd916670e42b18f7ea1db950b60044681c7
diegotimao <diegotimao1104@gmail.com>
2023-01-08 08:07:50 -0500

    feat: Implentando classe CompleteReport

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,13 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from typing import List, Dict
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(list: List[Dict]):
+        simple = SimpleReport.generate(list)
+        companhia = []
+
+        for item in list:
+            companhia.append(item["nome_da_empresa"])
+            print(companhia)
