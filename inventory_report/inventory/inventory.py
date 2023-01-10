981552d3169defafc02a5d653fd5eb2ab66bcb97
diegotimao <diegotimao1104@gmail.com>
2023-01-10 11:59:35 -0500

    Implementando metodo de buscar um arquivo csv e chamar a class que gera o relatório

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,7 +1,20 @@
+import csv
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+
+
 class Inventory:
     @staticmethod
+    def get_file_csv(path):
+        with open(path, "r", encoding="utf8") as arquivo:
+            arquivo_csv = csv.DictReader(arquivo, delimiter=",")
+            return [row for row in arquivo_csv]
+
+    @staticmethod
     def import_data(path, type):
+        list = Inventory.get_file_csv(path)
+
         if type == "simples":
-            print("simples")
+            return SimpleReport.generate(list)
         else:
-            print("completo")
+            return CompleteReport.generate(list)
