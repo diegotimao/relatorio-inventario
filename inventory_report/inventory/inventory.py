a4342520ceb11081ef55c278c5e7805fe22fc30f
diegotimao <diegotimao1104@gmail.com>
2023-01-11 09:37:32 -0500

    feat: Lendo arquivo .json dentro da classe Inventory

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,4 +1,6 @@
 import csv
+import json
+import os
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 
@@ -11,10 +13,32 @@ class Inventory:
             return [row for row in arquivo_csv]
 
     @staticmethod
-    def import_data(path, type):
-        list = Inventory.get_file_csv(path)
+    def get_file_json(path):
+        with open(path) as file:
+            file_list = json.load(file)
+            return file_list
 
-        if type == "simples":
-            return SimpleReport.generate(list)
-        else:
-            return CompleteReport.generate(list)
+    @staticmethod
+    def type_filename(path):
+        arquivo, extensao = os.path.splitext(path)
+        return extensao
+
+    @staticmethod
+    def import_data(path, type):
+        extension = Inventory.type_filename(path)
+        if extension == ".csv":
+            list = Inventory.get_file_csv(path)
+
+            if type == "simples":
+                return SimpleReport.generate(list)
+            else:
+                return CompleteReport.generate(list)
+        elif extension == ".json":
+            file_list = Inventory.get_file_json(path)
+
+            if type == "simples":
+                return SimpleReport.generate(file_list)
+            else:
+                return CompleteReport.generate(file_list)
+        elif extension == ".xml":
+            print("Arquivo de extensão xml", extension)
