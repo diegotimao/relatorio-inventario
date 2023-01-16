16449e8f3bc91f42f596f6e01a3a1f0badb7bf91
diegotimao <diegotimao1104@gmail.com>
2023-01-16 11:40:05 -0500

    feat: Testando o retorno da classe Product

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,23 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+assertions = (
+    "O produto farinha"
+    " fabricado em 01-05-2021"
+    " por Farinini com validade"
+    " até 02-06-2023"
+    " precisa ser armazenado ao abrigo de luz."
+)
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "farinha",
+        "Farinini",
+        "01-05-2021",
+        "02-06-2023",
+        "123456",
+        "ao abrigo de luz",
+    )
+
+    assert str(product) == assertions
