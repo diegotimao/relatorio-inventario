a32f25df190e5fb3391018a56bc65f69593dfc91
diegotimao <diegotimao1104@gmail.com>
2023-01-18 11:05:31 -0500

    feat: Adicionando a classe InventoryIterator

--- inventory_report/inventory/inventory_iterator.py
+++ inventory_report/inventory/inventory_iterator.py
@@ -1,1 +1,16 @@
+from collections.abc import Iterator
 
+
+class InventoryIterator(Iterator):
+    def __init__(self, prod):
+        self.prod = prod
+        self.index = 0
+
+    def __next__(self):
+        try:
+            products = self.prod[self.index]
+        except IndexError:
+            raise StopIteration()
+        else:
+            self.index += 1
+            return products
